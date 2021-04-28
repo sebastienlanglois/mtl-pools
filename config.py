0a3401a068e60c62609a2c316c1ec9dc196877c0
sebastienlanglois <sebastien.langlois@polymtl.ca>
2021-04-28 19:12:58 -0400

    user config update

--- config.py
+++ config.py
@@ -6,21 +6,29 @@ import pytz
 class Config(object):
 
     # User-defined
-    schedule = "Samedi 18:00 - 19:00"
-    query = "piscine"
-    delay_register_days = 3
+    schedules = ["Mardi 17:30 - 18:30",
+                 "Jeudi 17:30 - 18:30"]
+    query = "Centre du plateau"
+    delay_register_days = 2
+    #
 
     # Constants
-    day = dateparser.parse(u"{}".format(schedule.split()[0])).date()
-    if dateparser.parse(u"{}".format(schedule.split()[0])).date() <= datetime.today().date():
-        day = day + timedelta(days=7)
-    day_of_week = str((day.weekday() + 1) % 7)
+    for schedule in schedules:
+        day = dateparser.parse(u"{}".format(schedule.split()[0])).date()
+        if dateparser.parse(u"{}".format(schedule.split()[0])).date() <= datetime.today().date():
+            day = day + timedelta(days=7)
+        day_of_week = str((day.weekday() + 1) % 7)
 
-    datetime_to_register = pytz.timezone('America/Montreal') \
-        .localize(datetime.strptime((day - timedelta(days=delay_register_days)).strftime("%Y-%m-%d") + ' ' + schedule.split()[1], '%Y-%m-%d %H:%M')) \
-        .astimezone(pytz.utc)
+        datetime_to_register = pytz.timezone('America/Montreal') \
+            .localize(datetime.strptime((day - timedelta(days=delay_register_days)).strftime("%Y-%m-%d") + ' ' + schedule.split()[1], '%Y-%m-%d %H:%M')) \
+            .astimezone(pytz.utc)
 
-    ready_to_register = True if (datetime_to_register - datetime.now(timezone.utc)) < timedelta(minutes=14) else False
+        delta_time = (datetime_to_register - datetime.now(timezone.utc)).total_seconds()
+        if (delta_time > 0) and (delta_time < 14*60):
+            ready_to_register = True
+            break
+        else:
+            ready_to_register = False
 
     website_url = "https://loisirs.montreal.ca/IC3/#/U2010"
     table_activities_url = 'https://loisirs.montreal.ca/IC3/#/U5200/search/?searchParam='
